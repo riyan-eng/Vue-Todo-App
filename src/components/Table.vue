47b959fd30d64a9e99c211580258ec6f6fd81561
riyan-eng <fbriyant11@gmail.com>
2022-11-19 12:35:59 +0700

    add component

---
+++ src/components/Table.vue
@@ -0,0 +1,47 @@
+<template lang="">
+    <div>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>
+                    <router-link to="/detail">
+                      <button>Detail</button>
+                    </router-link>
+                    <router-link to="/update">
+                      <button>Update</button>
+                    </router-link>
+                    <button @click="onDelete">Delete</button>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>@fat</td>
+                </tr>
+             
+              </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    name: "Table",
+    methods:{
+      onDelete(){
+        console.log("hapus")
+      }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div class="container" style="max-width:600px">
+        <h2 class="text-center mt-5">My vue todo app</h2>
+        <AddForm/>
+        <div class="mt-3">
+            <Table/>
+        </div>
+    </div>
+</template>
+<script>
+import AddForm from "./Add.vue"
+import Table from "./Table.vue"
+
+export default {
+    components:{
+        AddForm,
+        Table
+    },
+    name: 'TodoApp',
+}
+</script>
+<style lang="">
+    
+</style>
