e421596cc0496d3877e5c8f48dacc9f56597279f
riyan-eng <fbriyant11@gmail.com>
2022-11-19 13:48:11 +0700

    todo

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,22 +1,54 @@
 <template lang="">
     <div class="container" style="max-width:600px">
         <h2 class="text-center mt-5">My vue todo app</h2>
-        <AddForm/>
+        <div>
+            <form @submit.prevent="addTodo">
+                <label for="newTodo">New Todo</label>
+                <input v-model="newTodo" type="text" name="newTodo" id="newTodo">
+                <button type="submit" name="button">Add</button>
+            </form>
+        </div>
+        <button @click="allDone" type="button" name="button">All Done</button>
         <div class="mt-3">
-            <Table/>
+            <ul>
+                <li v-for="todo in todos">
+                  <input type="checkbox" v-model="todo.done">
+                  <span :class="{ done: todo.done }">{{todo.title}}</span>
+                  <button @click="removeTodo(todo)" type="button" name="button">Remove</button>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
 <script>
-import AddForm from "./Add.vue"
-import Table from "./Table.vue"
 
 export default {
-    components:{
-        AddForm,
-        Table
-    },
     name: 'TodoApp',
+    data(){
+        return {
+            title: 'Hello Coding Garden',
+            newTodo: '',
+            todos: []
+        }
+    },
+    methods: {
+        addTodo() {
+          this.todos.push({
+            title: this.newTodo,
+            done: false
+          });
+          this.newTodo = '';
+        },
+        removeTodo(todo) {
+          const todoIndex = this.todos.indexOf(todo);
+          this.todos.splice(todoIndex, 1);
+        },
+        allDone() {
+          this.todos.forEach(todo => {
+            todo.done = true;
+          });
+        }
+      }
 }
 </script>
 <style lang="">
